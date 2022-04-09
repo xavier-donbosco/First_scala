c2da02c4febe995d25bbb071630a1cda7a1cf790
xavier-donbosco <xavier.donbosco@diggibyte.com>
2022-04-09 09:37:59 +0530

    first commit

---
+++ lecture2/oop/inheritance.scala
@@ -0,0 +1,16 @@
+package lecture2.oop
+
+object inheritance extends App {
+  val beast=new Beast
+  println(beast.creatureType)
+  println(beast.eat)
+
+}
+abstract class Animal{
+  val creatureType: String
+  def eat: Unit
+}
+class Beast extends Animal{
+  val creatureType="Bird"
+  def eat=println("Crunch crunch")
+}
