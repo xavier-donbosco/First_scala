3be2107a4433f596e8e954905a8d74df5ba6f2f0
xavier-donbosco <xavier.donbosco@diggibyte.com>
2022-04-20 08:03:15 +0530

    updating the files

--- lecture2/oop/oobasics.scala
+++ lecture2/oop/oobasics.scala
@@ -8,6 +8,7 @@ object oobasics extends App {
 }
 class Writer(fname: String, sname: String, year: Int){
   def fullname(): Unit=println("Writer name is "+fname+" "+sname)}
+
 class Novel(name: String, y_r: Int, author: Unit){
   def age(): Unit="Your age is "+(2000-y_r)
   def iswrittenby():Unit="Author name is " + this.author
